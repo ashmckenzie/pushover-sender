15679c6b6bbb405a48814b582091592e2a343f17
Ash McKenzie <ash@greenworm.com.au>
2013-12-13 10:26:21 +1100

    Added --version

--- lib/pushover_sender/command.rb
+++ lib/pushover_sender/command.rb
@@ -1,6 +1,11 @@
 module PushoverSender
   class Command < Clamp::Command
 
+    option "--version", :flag, "show version" do
+      puts "pushover-sender #{PushoverSender::VERSION}"
+      exit 0
+    end
+
     option '--title', 'TITLE', 'Title of message', required: true
     parameter 'message', 'Message content', required: true
 
